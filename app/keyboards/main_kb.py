97702f6064ad69381381b3318f7a112f3b4a6b59
Timofeyyy1 <159601541+Timofeyyy1@users.noreply.github.com>
2025-06-05 06:53:09 +0300

    Update main_kb.py

--- app/keyboards/main_kb.py
+++ app/keyboards/main_kb.py
@@ -1,10 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def get_content_plan_actions_keyboard() -> InlineKeyboardMarkup:
-    """
-    Клавиатура после генерации контент-плана.
-    Предлагает создать пост для первого дня.
-    """
     buttons = [
         [
             InlineKeyboardButton(text="Создать пример поста 💻", callback_data="generate_example_post")
@@ -12,4 +8,3 @@ def get_content_plan_actions_keyboard() -> InlineKeyboardMarkup:
     ]
     return InlineKeyboardMarkup(inline_keyboard=buttons)
 
-# get_post_generation_actions_keyboard удалена, так как не используется
